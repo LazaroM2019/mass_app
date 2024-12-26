1d474b2f042a22612ab899a0e086765c175f0808
LazaroM2019 <54071975+LazaroM2019@users.noreply.github.com>
2024-12-25 21:10:48 -0300

    Changed timezome from utc to montevideo timezone (#3)
    * Updated requirements file
    
    * Changed timezone from utc to montevideo

--- services/whatsapp.py
+++ services/whatsapp.py
@@ -35,6 +35,36 @@ def send_whatsapp_message(number, formatted_message):
     except Exception as e:
         return {"status": "failed", "error": str(e)}
 
+# def send_whatsapp_message(number, formatted_message, image_base64=None, caption=None):
+#     # Build the payload based on whether an image is provided
+#     if image_base64 is not None:
+#         payload = {
+#             "messaging_product": "whatsapp",
+#             "to": number,
+#             "type": "image",
+#             "image": {
+#                 "data": image_base64,  # Base64-encoded image data
+#                 "caption": caption or formatted_message  # Use caption or formatted_message as a fallback
+#             }
+#         }
+#     else:
+#         payload = {
+#             "messaging_product": "whatsapp",
+#             "to": number,
+#             "type": "text",
+#             "text": {"body": formatted_message}
+#         }
+    
+#     try:
+#         response = requests.post(URL_WHATSAPP, headers=HEADERS, json=payload)
+#         if response.status_code == 200:
+#             return {"status": "success", "message_sid": response.json()}
+#         else:
+#             return {"status": "failed", "error": response.text}
+#     except Exception as e:
+#         return {"status": "failed", "error": str(e)}
+
+
 # Function to schedule a WhatsApp message
 def schedule_whatsapp_message(title, message, numbers, send_time):
     formatted_message = f"*{title}*\n\n{message}"
@@ -45,3 +75,19 @@ def schedule_whatsapp_message(title, message, numbers, send_time):
             run_date=send_time,
             args=[number, formatted_message]
         )
+
+# Function to schedule a WhatsApp message
+# def schedule_whatsapp_message(title, message, numbers, send_time, image_base64=None, caption=None):
+#     # Format the message
+#     formatted_message = f"*{title}*\n\n{message}"
+    
+#     for number in numbers:
+#         # Schedule the send_whatsapp_message function with appropriate arguments
+#         scheduler.add_job(
+#             send_whatsapp_message,
+#             'date',
+#             run_date=send_time,
+#             args=[number, formatted_message, image_base64, caption]
+#         )
+#     print(f"Message scheduled for {len(numbers)} recipient(s) at {send_time}")
+
