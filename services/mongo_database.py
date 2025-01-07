6fe3591cb76fafdad3cdaa085e4ce891a4167ee2
abelpenton <32851047+abelpenton@users.noreply.github.com>
2025-01-07 20:02:16 -0300

    use user id for messages, and send only text template (#5)
    * use user id for messages, and send only text template
    
    * load token from env

--- services/mongo_database.py
+++ services/mongo_database.py
@@ -1,4 +1,61 @@
+from pymongo import MongoClient
+import os
 
+class MongoDBService:
+    def __init__(self):
+        self.client = MongoClient("mongodb+srv://inland:9sFRBksRbIgC4htj@cluster0.tha5gev.mongodb.net/inland_data_uy?retryWrites=true&w=majority")
+        self.database = self.client["inland_data_uy"]
+
+    def get_document_by_filter(self, collection_name: str, filter: dict):
+        collection = self.database[collection_name]
+        document = collection.find_one(filter)
+        return document
+
+    def write_to_collection(self, collection_name: str, data: dict):
+        collection = self.database[collection_name]
+        result = collection.insert_one(data)
+        return result.inserted_id
+
+    def upsert_to_collection(self, collection_name: str, query, update):
+        collection = self.database[collection_name]
+        
+        result = collection.update_one(query, update, upsert=True)
+        
+        return result.upserted_id if result.upserted_id else result.modified_count
+    
+    def close_connection(self):
+        self.client.close()
+
+
+def add_chat_message(user_id, number, text, date, is_client):
+    mongo_service = MongoDBService()
+
+    query = {"userId": user_id, "number": number}
+        
+    update = {
+        "$set": {"userId": user_id, "number": number},
+        "$push": {"messages": {
+            "text": text,
+            "date": date,
+            "is_client": is_client
+        }}
+    }
+    
+    mongo_service.upsert_to_collection("chats_history", query, update)
+    
+
+def get_whatsapp_credentials(user_id):
+    mongo_service = MongoDBService()
+
+    filter = {"_id": user_id} 
+
+    user = mongo_service.get_document_by_filter("users", filter)
+
+    if user is not None and user.whatsapp_id is not None:
+        return user.whatsapp_id
+    
+    WHATSAPP_ACCOUNT_SID = os.getenv('WHATSAPP_ACCOUNT_SID')
+    return WHATSAPP_ACCOUNT_SID
 
 def save_to_mongodb(database, collection_name, data):
     """
--- services/whatsapp.py
+++ services/whatsapp.py
@@ -2,17 +2,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 import os
 from utils.logger import logger
+from datetime import datetime, timezone
+from services.mongo_database import add_chat_message, get_whatsapp_credentials
 
 # Initialize the scheduler (ensure it's started only once)
 scheduler = BackgroundScheduler()
 scheduler.start()
 
 # WHATSAPP credentials saved
-WHATSAPP_ACCOUNT_SID = os.getenv('WHATSAPP_ACCOUNT_SID')
 WHATSAPP_AUTH_TOKEN = os.getenv('WHATSAPP_AUTH_TOKEN')
-
-# WHATSAPP API ENDPOINT
-URL_WHATSAPP = f"https://graph.facebook.com/v21.0/{WHATSAPP_ACCOUNT_SID}/messages"
 
 HEADERS = {
     "Authorization": f"Bearer {WHATSAPP_AUTH_TOKEN}",
@@ -20,7 +18,7 @@ HEADERS = {
 }
 
 # Function to send a WhatsApp message
-def send_whatsapp_message(number, title_front, text_front):
+def send_whatsapp_message(user_id, number, title_front, text_front):
     title = title_front.replace("\n", "")
     message = text_front.replace("\n", "").replace("\r", "")
     payload = {
@@ -35,16 +33,29 @@ def send_whatsapp_message(number, title_front, text_front):
             "components": [
                 {
                     "type": "header",
-                    "parameters": [{"type": "text","text": title}]
+                    "parameters": [{"type": "text", "text": title}]
                 },
                 {
                     "type": "body",
                     "parameters": [{"type": "text", "text": message}]
                 }
-                ]
+            ]
         }
     }
+
+    if title == "chat_only":
+        add_chat_message(user_id, number, message, datetime.now(timezone.utc), False)
+        payload["template"]["name"] = "text_dynamic_message"
+        payload["template"]["components"] = [
+            {
+                "type": "body",
+                "parameters": [{"type": "text", "text": message}]
+            }
+        ]
+
     try:
+        account_id = get_whatsapp_credentials(user_id)
+        URL_WHATSAPP = f"https://graph.facebook.com/v21.0/{account_id}/messages"
         response = requests.post(URL_WHATSAPP, headers=HEADERS, json=payload)
         if response.status_code == 200:
             return {"status": "success", "message_sid": response.json()}
@@ -84,14 +95,14 @@ def send_whatsapp_message(number, title_front, text_front):
 
 
 # Function to schedule a WhatsApp message
-def schedule_whatsapp_message(title, message, numbers, send_time):
+def schedule_whatsapp_message(user_id, title, message, numbers, send_time):
     formatted_message = f"*{title}*\n\n{message}"
     for number in numbers:
         scheduler.add_job(
             send_whatsapp_message,
             'date',
             run_date=send_time,
-            args=[number, title, message]
+            args=[user_id, number, title, message]
         )
 
 # Function to schedule a WhatsApp message
